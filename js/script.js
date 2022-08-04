08131e894e4c27827cefaad0480055a611fa4242
willywin99 <willywinsianturi@gmail.com>
2022-08-04 08:32:32 +0700

    Menampilkan Item Todo yang Selesai dan Menghapus Todo

--- js/script.js
+++ js/script.js
@@ -34,14 +34,18 @@ function generateTodoObject(id, task, timestamp, isCompleted) {
 }
 
 document.addEventListener(RENDER_EVENT, function () {
-  // console.log(todos);
   const uncompletedTODOList = document.getElementById('todos');
   uncompletedTODOList.innerHTML = '';
+
+  const completedTODOList = document.getElementById('completed-todos');
+  completedTODOList.innerHTML = '';
 
   for (const todoItem of todos) {
     const todoElement = makeTodo(todoItem);
     if (!todoItem.isCompleted) {
       uncompletedTODOList.append(todoElement);
+    } else {
+      completedTODOList.append(todoElement);
     }
   }
 });
@@ -108,4 +112,32 @@ function findTodo (todoId) {
     }
   }
   return null;
+}
+
+function removeTaskFromCompleted(todoId) {
+  const todoTarget = findTodoIndex(todoId);
+
+  if (todoTarget === -1) return;
+
+  todos.splice(todoTarget, 1);
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function undoTaskFromCompleted(todoId) {
+  const todoTarget = findTodo(todoId);
+
+  if (todoTarget == null) return;
+
+  todoTarget.isCompleted = false;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function findTodoIndex(todoId) {
+  for (const index in todos) {
+    if (todos[index].id === todoId) {
+      return index;
+    }
+  }
+
+  return -1;
 }
